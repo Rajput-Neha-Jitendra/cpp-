32f9daba88ccbcfc89aa18d9059e83cd8ceb7e86
Rajput Neha Jitendra <100520546+Rajput-Neha-Jitendra@users.noreply.github.com>
2022-04-24 06:46:15 +0530

    Loop-For-Loop

---
+++ 12For_Loop.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+        for (int i = 1; i <= 10; ++i) {
+        cout << i << "\n ";
+    }
+    return 0;
+}
+
+/*C++ Program to display a text 5 times
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 5; ++i) {
+        cout <<  "Hello Git-Hub! " << endl;
+    }
+    return 0;
+}
+// C++ program to find the sum of first n natural numbers
+// positive integers such as 1,2,3,...n are known as natural numbers
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num, sum;
+    sum = 0;
+
+    cout << "Enter a positive integer: ";
+    cin >> num;
+
+    for (int i = 1; i <= num; ++i) {
+        sum += i;
+    }
+
+    cout << "Sum = " << sum << endl;
+
+    return 0;
+}*/
+    
